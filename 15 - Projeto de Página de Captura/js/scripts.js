378bbced109df6ea90ef500cdf5a64962915dda4
yagosouza7 <ymdesouza@gmail.com>
2024-06-09 23:10:17 -0300

    Initial commit

---
+++ 15 - Projeto de Página de Captura/js/scripts.js
@@ -0,0 +1,11 @@
+var botoes = document.querySelectorAll(".js-botao");
+
+botoes[0].addEventListener("click", function () {
+  document.body.classList.toggle("popup--aberto");
+});
+botoes[1].addEventListener("click", function () {
+  document.body.classList.toggle("popup--aberto");
+});
+botoes[2].addEventListener("click", function () {
+  document.body.classList.toggle("popup--aberto");
+});
